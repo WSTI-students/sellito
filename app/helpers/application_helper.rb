9432d7c2958f79ccf288c5926b64365026fb51ae
Patryk Stępień <droznyk@gmail.com>
2017-05-06 18:54:02 +0200

    extract conditional statements from header partial file to helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def current_user_is_admin
+    current_user && current_user.is_admin?
+  end
+
+  def sign_up_user_link
+    link_to "Sign up", new_user_registration_path
+  end
+
+  def login_or_logout_link
+    current_user ? logout_user_link : login_user_link
+  end
+
+  def login_user_link
+    link_to "Log in", new_user_session_path
+  end
+
+  def logout_user_link
+    link_to 'Logout', destroy_user_session_path, method: :delete
+  end
 end
