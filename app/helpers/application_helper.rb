b5cd1ca5f4b15d71e8067e9e6963bb584643c058
Patryk Stępień <droznyk@gmail.com>
2017-05-09 20:41:37 +0200

    move actions from view to helper, combine log in/sign up methods into one

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,14 +8,20 @@ module ApplicationHelper
   end
 
   def login_or_logout_link
-    current_user ? logout_user_link : login_user_link
+    current_user ? logout_user_link : login_signup_user_link
   end
 
-  def login_user_link
-    link_to "Log in", new_user_session_path
+  def login_signup_user_link
+    login_signup_tag = "<li>#{link_to "Log in", new_user_session_path}</li> <li>#{link_to "Sign up", new_user_registration_path}</li>"
+    login_signup_tag.html_safe
   end
 
   def logout_user_link
     link_to 'Logout', destroy_user_session_path, method: :delete
+  end
+
+  def show_categories_for_admin
+    categories_link_tag = "<li>#{link_to 'Categories', categories_path}</li>"
+    categories_link_tag.html_safe if current_user_is_admin
   end
 end
