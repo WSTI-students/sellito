62cfdcf8385dc9c2e1382dba60074a9449efbbf9
Patryk Stępień <droznyk@gmail.com>
2017-05-12 17:56:54 +0200

    refactor categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :fetch_category, only: [:show, :edit, :update, :destroy]
+
   def index
     @categories = Category.all
   end
@@ -18,22 +20,16 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def show
-    @category = Category.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @category = Category.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @category = Category.find(params[:id])
     @category.update_attributes(category_params)
     redirect_to @category
   end
 
   def destroy
-    @category = Category.find(params[:id])
     @category.destroy
     flash[:notice] = "Category #{@category.name} deleted"
     redirect_to categories_path
@@ -42,5 +38,9 @@ class CategoriesController < ApplicationController
   private
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def fetch_category
+    @category = Category.find(params[:id])
   end
 end
