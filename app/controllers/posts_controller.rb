876181ab54cc04c2c7fa7013258837e4e97b10fd
Patryk Stępień <droznyk@gmail.com>
2017-05-12 20:53:24 +0200

    add controller for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+
+
+
+  def index
+    @posts = current_user.posts
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    return unless post_params[:user_id]== current_user.id.to_s
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post created!'
+      redirect_to posts_path(@post)
+    else
+      flash[:alert] = 'Could not create post :('
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :expiration_date, :user_id)
+  end
+
+end
