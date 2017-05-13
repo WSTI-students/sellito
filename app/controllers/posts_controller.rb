ab7620216e76ca16ff82b4833e09e083def57ed3
Patryk Stępień <droznyk@gmail.com>
2017-05-13 10:20:13 +0200

    add show/edit/update/destroy methods to the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
-
-
+  before_action :fetch_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = current_user.posts
+    if current_user
+      @posts = current_user.posts
+    else
+      @posts = Post.all
+    end
   end
 
   def new
@@ -22,14 +25,29 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
+  def show; end
+
+  def edit; end
+
+  def update
+    @post.update_attributes(post_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @post.destroy
+    flash[:notice] = "Post #{@post.title} deleted"
+    redirect_to posts_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :description, :expiration_date, :user_id)
+  end
+
+  def fetch_post
+    @post = Post.find(params[:id])
   end
 
 end
