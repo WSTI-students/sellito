4e6f86e1258345c14a7a440be1658ef503a61fa7
Patryk Stępień <droznyk@gmail.com>
2017-05-14 14:38:42 +0200

    solve the n+1 problem

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,10 @@
 class HomepageController < ApplicationController
   def index;
     @categories = Category.all
+    # zmniejsza ilosc zapytan do bazy z n+1 do wymaganego minimum
+    #n+1 to nadmiarowa ilosc zapytan do bazy; niezoptymalizowane zapytanie do bazy, ktore wykonuje sie niepotrzebnie wiele razy
+    # przy wyswietlaniu posta idzie zapytanie do bazy o posty wraz z kategoriami
+    @posts = Post.all.includes(:categories)
   end
 
   def about; end
