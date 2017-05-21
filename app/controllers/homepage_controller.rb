50858a17bd784603f6850242872c10ed17205c76
Patryk Stępień <droznyk@gmail.com>
2017-05-21 14:02:28 +0200

    limit the number of listed posts on homepage from all to 10

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,9 +2,9 @@ class HomepageController < ApplicationController
   def index;
     @categories = Category.all
     # zmniejsza ilosc zapytan do bazy z n+1 do wymaganego minimum
-    #n+1 to nadmiarowa ilosc zapytan do bazy; niezoptymalizowane zapytanie do bazy, ktore wykonuje sie niepotrzebnie wiele razy
+    #n+1 to nadmiarowa ilosc zapytan do bazy; niezoptymalizowane zapytanie do bazy, ktore wykonuje sie niepotrzebnie wiele
     # przy wyswietlaniu posta idzie zapytanie do bazy o posty wraz z kategoriami
-    @posts = Post.all.includes(:categories)
+    @posts = Post.all.includes(:categories).order('created_at DESC').limit(10)
   end
 
   def about; end
