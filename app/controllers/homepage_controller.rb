69c2dd228fd39cb3f69eea5c5af939a49dcddd05
Patryk Stępień <droznyk@gmail.com>
2017-05-21 14:29:54 +0200

    refactor code; move active record methods for posts from controller to model

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,7 +4,7 @@ class HomepageController < ApplicationController
     # zmniejsza ilosc zapytan do bazy z n+1 do wymaganego minimum
     #n+1 to nadmiarowa ilosc zapytan do bazy; niezoptymalizowane zapytanie do bazy, ktore wykonuje sie niepotrzebnie wiele
     # przy wyswietlaniu posta idzie zapytanie do bazy o posty wraz z kategoriami
-    @posts = Post.all.includes(:categories).order('created_at DESC').limit(10)
+    @featured_posts = Post.featured_posts
   end
 
   def about; end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+  scope :featured_posts, lambda {
+                                  includes(:categories)
+                                    .order('created_at DESC')
+                                    .limit(10)
+                                }
   belongs_to :user
   has_many :post_categories
   has_many :categories, through: :post_categories
